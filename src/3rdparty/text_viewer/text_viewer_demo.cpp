b48d8c668baa02019e175b103407cfd4f45bf299
kevreco <6670943+kevreco@users.noreply.github.com>
2025-03-01 17:04:49 +0100

    Text Viewer: improve demo.

--- src/3rdparty/text_viewer/text_viewer_demo.cpp
+++ src/3rdparty/text_viewer/text_viewer_demo.cpp
@@ -12,6 +12,7 @@ namespace tv {
     static bool print_cursor_coord = true;
     static bool print_selected_line_number = true;
     static bool print_selected_text = true;
+    static bool print_selected_line_text = true;
 
     static std::string text;
 
@@ -54,6 +55,18 @@ int main() {
         }
 
         {
+            if (ImGui::CollapsingHeader("Features List"))
+            {
+                ImGui::BulletText("Current line selection (can be disabled).");
+                ImGui::BulletText("Current text selection (can be disabled).");
+                ImGui::BulletText("Line number (can be disabled).");
+                ImGui::BulletText("Custom widgets can be displayed before the line.");
+                ImGui::BulletText("Copy selection via Ctrl + C and Ctrl + X");
+                ImGui::BulletText("Move cursor with Ctrl + Up and Ctrl + Down");
+                ImGui::BulletText("Ctrl + A to select all the text");
+                ImGui::BulletText("Keyboards and mouse inputs can be disabled.");
+            }
+
             if (ImGui::CollapsingHeader("Text Viewer Options"))
             {
                 // Options
@@ -76,13 +89,13 @@ int main() {
                 ImGui::Checkbox("Display Cursor Coordinate", &print_cursor_coord);
                 ImGui::Checkbox("Display Selected Line Number Coordinate", &print_selected_line_number);
                 ImGui::Checkbox("Get Selected Text", &print_selected_text);
+                ImGui::Checkbox("Get Selected Line Text", &print_selected_line_text);
             }
-
 
             {
                 ImGuiWindowFlags window_flags = ImGuiWindowFlags_HorizontalScrollbar;
 
-                ImGui::BeginChild("ChildL", ImVec2(ImGui::GetContentRegionAvail().x * 0.5f, -FLT_MIN), ImGuiChildFlags_Borders | ImGuiChildFlags_FrameStyle, window_flags);
+                ImGui::BeginChild("LeftChild", ImVec2(ImGui::GetContentRegionAvail().x * 0.5f, -FLT_MIN), ImGuiChildFlags_Borders | ImGuiChildFlags_FrameStyle, window_flags);
 
                 viewer.render();
 
@@ -94,12 +107,12 @@ int main() {
             {
                 ImGuiWindowFlags window_flags = ImGuiWindowFlags_None;
 
-                ImGui::BeginChild("ChildR", ImVec2(0, -FLT_MIN), ImGuiChildFlags_Borders, window_flags);
+                ImGui::BeginChild("RightChild", ImVec2(0, -FLT_MIN), ImGuiChildFlags_Borders, window_flags);
                 if (print_selection_coord)
                 {
                     auto range = viewer.get_selection_range();
 
-                    ImGui::BulletText("Selection Range (%d %d) - (%d %d)",
+                    ImGui::BulletText("Selection Range: (%d %d) - (%d %d)",
                         range.start.line, range.start.column,
                         range.end.line, range.end.column);
                 }
@@ -107,23 +120,40 @@ int main() {
                 if (print_cursor_coord)
                 {
                     auto coord = viewer.get_cursor_position();
-                    ImGui::BulletText("Cursor Position %d %d", coord.line, coord.column);
+                    ImGui::BulletText("Cursor Position: (%d %d)", coord.line, coord.column);
                 }
 
                 if (print_selected_line_number)
                 {
                     auto line_number = viewer.get_selected_line_number();
-                    ImGui::BulletText("Selected line number %zu", line_number);
+                    ImGui::BulletText("Selected line number: %zu", line_number);
                 }
                 
                 if (print_selected_text)
                 {
-                    ImGui::BulletText("Selected Text:");
-                    ImGui::Separator();
-                    ImGui::Text(TV_SV_FMT, TV_SV_ARG(viewer.get_selected_line_text()));
-                    ImGui::Separator();
+                    ImGui::SeparatorText("Selected Text:");
+
+                    tv::coord_range range = viewer.get_selection_range();
+                    int draw_lines = range.end.line - range.start.line;
+                    if (draw_lines > 0)
+                    {
+                        ImGui::Text("Line count selection: %d", draw_lines);
+
+                        if (ImGui::BeginChild("Selected Text", ImVec2(-FLT_MIN, 0.0f), ImGuiChildFlags_Borders | ImGuiChildFlags_FrameStyle | ImGuiChildFlags_AutoResizeY))
+                            ImGui::Text("%s", viewer.get_selected_text().c_str());
+                        ImGui::EndChild();
+                    }
                 }
 
+                if (print_selected_line_text)
+                {
+                    ImGui::SeparatorText("Selected Line Text:");
+                    
+                    if (ImGui::BeginChild("Selected Line Text", ImVec2(-FLT_MIN, 0.0f), ImGuiChildFlags_Borders | ImGuiChildFlags_FrameStyle | ImGuiChildFlags_AutoResizeY))
+                        ImGui::Text(TV_SV_FMT, TV_SV_ARG(viewer.get_selected_line_text()));
+                    ImGui::EndChild();
+                    ImGui::Separator();
+                }
                 ImGui::EndChild();
             }
         }
