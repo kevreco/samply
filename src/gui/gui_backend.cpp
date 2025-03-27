2ee20b49bf6c4d499048305da10a11957f9a22c5
kevreco <6670943+kevreco@users.noreply.github.com>
2025-03-27 16:43:30 +0100

    Backend: Clean up some comments.

--- src/gui/gui_backend.cpp
+++ src/gui/gui_backend.cpp
@@ -1,14 +1,11 @@
 #include "gui.hpp"
 
-// Dear ImGui: standalone example application for Win32 + OpenGL 3
-
-// Learn about Dear ImGui:
-// - FAQ                  https://dearimgui.com/faq
-// - Getting Started      https://dearimgui.com/getting-started
-// - Documentation        https://dearimgui.com/docs (same as your local docs/ folder).
-// - Introduction, links and more at the top of imgui.cpp
-
-// This is provided for completeness, however it is strongly recommended you use OpenGL with SDL or GLFW.
+// This Dear imagui backend was a revamped of https://github.com/ocornut/imgui/blob/master/examples/example_win32_opengl3/main.cpp
+//
+// Likely an important comment from the original code:
+// 
+//    This is provided for completeness, however it is strongly recommended you use OpenGL with SDL or GLFW.
+//
 
 #if _WIN32
 #include "imgui_plus_extensions.h"
@@ -20,7 +17,6 @@
 #include <windows.h>
 #include <strsafe.h> /* StringCchVPrintfW */
 #include <GL/GL.h>
-//#include <GL/glext.h>
 #include <tchar.h>
 #include "stdio.h"
 
@@ -153,22 +149,6 @@ int gui_backend::show()
     ImGui::StyleColorsDark();
     ImGui::ApplyCustomTheme();
     ImGui::TryLoadCustomFonts();
-
-    // Load Fonts
-    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
-    // - If the file cannot be loaded, the function will return a nullptr. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-    // - Use '#define IMGUI_ENABLE_FREETYPE' in your imconfig file to use Freetype for higher quality font rendering.
-    // - Read 'docs/FONTS.md' for more instructions and details.
-    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-    //io.Fonts->AddFontDefault();
-    //io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\segoeui.ttf", 18.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
-    //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, nullptr, io.Fonts->GetGlyphRangesJapanese());
-    //IM_ASSERT(font != nullptr);
 
     // Our state
 
